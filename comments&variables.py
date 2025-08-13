4bd7ea6fd7a55e27f71039b2b68cef74878e26fe
Rishu <rishuchaudhary@gmail.com>
2025-08-13 12:31:31 +0530

    initial commit

--- comments&variables.py
+++ comments&variables.py
@@ -1,8 +1,15 @@
+"""Comments"""
 # Comments are something that are ignored by the python interpreter...
 # we have to use # for writing a comment in python....
 # Multiline comments are not available in python but we can achieve it by using Doc String """Multiline"""
 
-# Variables in python
+"""Variables in python"""
 # In python variables are used as a storage things in python (we will see later what we have to store). we can write anything as a variable namee
-# Name="Rii"
-# Age=20
+# name="Rii"
+# age=20
+
+# @name="rii" #error 
+
+# firstname="Rii"   #pascal case
+# firstName="Anii"  #camel case
+# first_name="Riiu"  #snake case
--- datatypes.py
+++ datatypes.py
@@ -1,14 +1,15 @@
+"""DataTypes"""
 # datatypes are the things we store in variables and it defines what data types variables are
 
 # there are different types of datatypes
 # Int, float, complex, bool, strings, bool,
 
 
-# a= 12
-# b=2.34
-# c=3+4j
-# d=True
-# e='rii'
+# a= 12  #int
+# b=2.34  #float
+# c=3+4j  #complex
+# d=True  #bool
+# e='rii'  #str
 # print(type(a))
 # print(type(b))
 # print(type(c))
@@ -22,7 +23,12 @@ a="Rii Anii"
 print(a[0:3:1])
 
 
-# Type conversion
+
+# num=24
+# print(24/3)  #it will result as a float number...
+
+
+"""Type conversion"""
 a=12
 a=str(a)
 print(type(a))
@@ -30,5 +36,6 @@ print(type(a))
 
 # Falsy values: false, 0, 0.0, " ", (), [], {}
 
+"""Types of type conversion"""
 # Implicit: In this python automatically converts data from one data type to another...
 # Explicit: In this we as a user use in build functions to convert one data type to another...
