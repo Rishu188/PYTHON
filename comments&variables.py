a8f543e3efaa6af8901bb01f1a7a556801576ea1
Rishu <rishuchaudhary@gmail.com>
2025-08-13 10:23:11 +0530

    Initial commit

---
+++ comments&variables.py
@@ -0,0 +1,8 @@
+# Comments are something that are ignored by the python interpreter...
+# we have to use # for writing a comment in python....
+# Multiline comments are not available in python but we can achieve it by using Doc String """Multiline"""
+
+# Variables in python
+# In python variables are used as a storage things in python (we will see later what we have to store). we can write anything as a variable namee
+# Name="Rii"
+# Age=20
---
+++ datatypes.py
@@ -0,0 +1,34 @@
+# datatypes are the things we store in variables and it defines what data types variables are
+
+# there are different types of datatypes
+# Int, float, complex, bool, strings, bool,
+
+
+# a= 12
+# b=2.34
+# c=3+4j
+# d=True
+# e='rii'
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(d))
+# print(type(e))
+
+
+
+# string slicing
+a="Rii Anii"
+print(a[0:3:1])
+
+
+# Type conversion
+a=12
+a=str(a)
+print(type(a))
+
+
+# Falsy values: false, 0, 0.0, " ", (), [], {}
+
+# Implicit: In this python automatically converts data from one data type to another...
+# Explicit: In this we as a user use in build functions to convert one data type to another...
