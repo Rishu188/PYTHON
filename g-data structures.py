4374a72f4702d3b8774108a903752534a5b48096
Rishu <rishuchaudhary@gmail.com>
2025-08-18 21:18:07 +0530

    initial commit

---
+++ g-data structures.py
@@ -0,0 +1,28 @@
+# Data  structures are used to store, organize, and manipulate data efficiently. Python provides several built-in data structures..
+
+# And for storing multiple values we will again use variables..
+
+"""IN-BUILD DATA STRUCTURES"""
+# Now in python we have 4-types of in-built data structure..
+# LIST
+# TUPLE
+# DICTIONARY 
+# SET..
+
+
+"""CUSTOM DATA STRUCTURES"""
+# STACK
+# QUEUE
+# LINKED LIST
+# GRAPH 
+
+# Around these data structure there are some algorithms like searching algorithms, sorting algo...
+
+"""List"""
+# Mutable : changed after creation
+# Duplicates : used to store multiple values so duplicates means same value occuring multiple time. list allows this...
+# Ordered : List maintains ordered data structure maintains the sequence of elements as they were inserted . This means you can access elements using their position (index)...
+# Heterogenous : List have heterogenous nature that means we can have multiple data types inside the list...  
+
+
+a=[12,11.14,15]
